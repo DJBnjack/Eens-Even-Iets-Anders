9c989272dc8a9ca702495399602b9ae27c0c140c
Johannes <johannes.bertens@gmail.com>
2016-04-20 13:09:12 +0200

    First hello from websockets

--- server.py
+++ server.py
@@ -1,13 +1,28 @@
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
+
+cl = []
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("wwwroot/index.html")
 
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+        
 def make_server():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r'/', MainHandler),
+        (r'/websocket', EchoWebSocket),
+        (r'/(.*)', tornado.web.StaticFileHandler, {'path': './wwwroot/'}),
     ])
 
 if __name__ == "__main__":
